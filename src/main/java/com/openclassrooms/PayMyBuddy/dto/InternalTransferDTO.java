ab0a7e2fc7a8f302e5e46e4d058bc603fcf8890b
artificialideas <amateuc@gmail.com>
2023-02-19 15:07:52 +0100

    Done: internalTransfer Round up amount (and add currency symbol)

--- src/main/java/com/openclassrooms/PayMyBuddy/dto/InternalTransferDTO.java
+++ src/main/java/com/openclassrooms/PayMyBuddy/dto/InternalTransferDTO.java
@@ -3,6 +3,8 @@ package com.openclassrooms.PayMyBuddy.dto;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Currency;
 
 @Data
 public class InternalTransferDTO {
@@ -10,4 +12,12 @@ public class InternalTransferDTO {
     private ContactDTO receiver;
     private BigDecimal amount;
     private String description;
+
+    public String getAmount() {
+        Currency currency = Currency.getInstance("EUR");
+        return amount.setScale(2, RoundingMode.HALF_EVEN) + currency.getSymbol();
+    }
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
 }
